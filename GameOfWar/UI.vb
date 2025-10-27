244e753261683e49084f1052ab70605b448cb927
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-10-27 17:55:37 -0600

    Add project files.

---
+++ GameOfWar/UI.vb
@@ -0,0 +1,13 @@
+﻿'Angel Nava
+'Spring 2025
+'RCET3371
+'GameOfWar UI
+'github link
+Option Strict On
+Option Explicit On
+
+Public Class UI
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
